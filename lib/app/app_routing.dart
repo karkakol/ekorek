75978b5ddac834f2b8b0a2a0d91612bcf3e22531
xRKowalczyk <rafalkowalczyk215@gmail.com>
2022-05-29 12:03:49 +0200

    EK-14 welcome screen

--- lib/app/app_routing.dart
+++ lib/app/app_routing.dart
@@ -1,5 +1,6 @@
 import 'package:ekorek/screen/auth/sign_in/sign_in_screen.dart';
 import 'package:ekorek/screen/auth/sign_up/sign_up_screen.dart';
+import 'package:ekorek/screen/auth/welcome/welcome_screen.dart';
 import 'package:ekorek/screen/home/home_screen.dart';
 import 'package:ekorek/screen/splash/splash_screen.dart';
 import 'package:utopia_arch/utopia_arch.dart';
@@ -10,6 +11,7 @@ class AppRouting {
     SignUpScreen.route: SignUpScreen.routeConfig,
     SignInScreen.route: SignInScreen.routeConfig,
     HomeScreen.route: HomeScreen.routeConfig,
+    WelcomeScreen.route: WelcomeScreen.routeConfig,
   };
 
   static const initialRoute = SplashScreen.route;
--- lib/screen/splash/state/use_splash_screen_state.dart
+++ lib/screen/splash/state/use_splash_screen_state.dart
@@ -1,6 +1,6 @@
 import 'package:ekorek/app/state/auth/auth_state.dart';
 import 'package:ekorek/app/state/setup/use_async_after_setup.dart';
-import 'package:ekorek/screen/auth/sign_in/sign_in_screen.dart';
+import 'package:ekorek/screen/auth/welcome/welcome_screen.dart';
 import 'package:ekorek/screen/home/home_screen.dart';
 import 'package:utopia_hooks/utopia_hooks.dart';
 import 'package:utopia_utils/utopia_utils_extensions.dart';
@@ -17,15 +17,16 @@ SplashScreenState useSplashScreenState() {
     await context.navigator.pushNamedAndRemoveUntil(HomeScreen.route, (_) => false);
   }
 
-  Future<void> navigateToSignIn() async {
-    await context.navigator.pushNamedAndRemoveUntil(SignInScreen.route, (_) => false);
+  Future<void> navigateToWelcome() async {
+    await context.navigator.pushNamedAndRemoveUntil(WelcomeScreen.route, (_) => false);
   }
 
   useAsyncEffectAfterSetup(() async {
     if (authState.user != null)
       navigateToHome();
     else
-      navigateToSignIn();
+      navigateToWelcome();
   });
+
   return SplashScreenState();
 }
