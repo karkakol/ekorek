0dba8504970bb685d0c3f702df5d5627e0343a03
Bartek <kalemba128@gmail.com>
2022-05-29 16:17:58 +0200

    EK-19 refactor auth state

--- lib/app/state/auth/auth_state.dart
+++ lib/app/state/auth/auth_state.dart
@@ -1,10 +1,11 @@
+import 'package:ekorek/app/state/auth/auth_status.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:utopia_hooks/utopia_hooks.dart';
 
 class AuthState {
-  final User? user;
-  const AuthState({required this.user});
+  final AuthStatus status;
+  const AuthState({required this.status});
 }
 
 class AuthStateProvider extends HookStateProviderWidget<AuthState> {
@@ -12,12 +13,14 @@ class AuthStateProvider extends HookStateProviderWidget<AuthState> {
 
   @override
   AuthState use() {
-    final userState = useState<User?>(FirebaseAuth.instance.currentUser);
+    getStatus(User? user) => user != null ? AuthStatus.AUTHORIZED : AuthStatus.UNAUTHORIZED;
+    final statusState = useState<AuthStatus>(getStatus(FirebaseAuth.instance.currentUser));
 
-    useStreamSubscription<User?>(FirebaseAuth.instance.authStateChanges(), (user) {
-      userState.value = user;
-    });
+    useStreamSubscription<User?>(
+      FirebaseAuth.instance.authStateChanges(),
+      (user) => statusState.value = getStatus(user),
+    );
 
-    return AuthState(user: userState.value);
+    return AuthState(status: statusState.value);
   }
 }
