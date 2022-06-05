3482e0ce98804a2f9b322ddc1ed10a582a287c14
Karol <kakol.karol00@gmail.com>
2022-06-05 13:53:28 +0200

    create tutortile in searchpage

---
+++ lib/screen/home/widgets/tabs/widget/tutor_tile.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ekorek/model/user/user.dart' as model;
+
+class TutorTile extends StatelessWidget {
+  final model.User tutor;
+  const TutorTile({Key? key, required this.tutor,}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(tutor.fullName),
+      ),
+    );
+  }
+}
