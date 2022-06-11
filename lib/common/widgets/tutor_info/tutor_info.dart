3db418717d42acb1c8bce39edc9b56279829e84d
Karol <kakol.karol00@gmail.com>
2022-06-11 20:09:36 +0200

    bl

--- lib/common/widgets/tutor_info/tutor_info.dart
+++ lib/common/widgets/tutor_info/tutor_info.dart
@@ -1,11 +1,13 @@
-import 'package:ekorek/model/user/user.dart' as model;
+import 'package:ekorek/config/app_colors.dart';
+import 'package:ekorek/model/user/user.dart' ;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import '../../../model/user/user.dart';
 
 class TutorInfo extends StatelessWidget {
-  final model.UserTutor tutor;
+  final UserTutor tutor;
   final String subject;
 
   const TutorInfo({
@@ -17,18 +19,20 @@ class TutorInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      leading: const Icon(Icons.school, color: AppColors.primaryColor),
+      contentPadding: const EdgeInsets.all(24),
       title: Text(tutor.fullName),
-      subtitle: Text(tutor.city),
+      subtitle: Text(subject),
       trailing: FittedBox(
         fit: BoxFit.fill,
         child: Row(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text(tutor.subjects[subject].toString()),
+            Text(NumberFormat.currency(locale: 'PL').format(tutor.subjects[subject])),
             const SizedBox(width: 4),
             const Icon(
               Icons.monetization_on_outlined,
-              color: Colors.green,
+              color: AppColors.primaryColor,
             ),
           ],
         ),
